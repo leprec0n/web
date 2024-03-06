0b49832decc1739e4cef5319e485eaffeca6c818
jarnoweemen <jarno.weemen@gmail.com>
2024-03-06 20:13:21 +0100

    Removed dynamic config variables

--- example.config.js
+++
@@ -1,5 +0,0 @@
-const config = {
-    apiUrl: 'http://127.0.0.1:8080',
-};
-
-export default config;
--- www/scripts/dynamic.js
+++
@@ -1,8 +0,0 @@
-import config from "../config.js";
-
-document.addEventListener("DOMContentLoaded", function () {
-    var dynamicButton = document.getElementById("content");
-    var baseUrl = config.apiUrl;
-    var path = "/";
-    dynamicButton.setAttribute("hx-get", baseUrl + path);
-});
