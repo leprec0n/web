558a5d6ce012ee30219b08f2d478005ba9c03ad6
jarnoweemen <jarno.weemen@gmail.com>
2024-04-01 16:45:11 +0200

    Set env variables through nginx

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@ set -e
 
 # Replace ENV placeholders with values
 envsubst < /etc/nginx/conf.d/server.conf.template > /etc/nginx/conf.d/server.conf
+envsubst < /usr/local/openresty/nginx/html/js/auth0.template.js > /usr/local/openresty/nginx/html/js/auth0.js
 
 # In order for NGINX to run from docker file
 exec "$@"
