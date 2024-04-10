5a2c869eb647a1c3a8bc6c9c8227cc3ff9d0678f
jarnoweemen <jarno.weemen@gmail.com>
2024-04-10 15:26:34 +0200

    Verify token

--- nginx/lua/authorise.lua
+++ nginx/lua/authorise.lua
@@ -15,15 +15,23 @@ for k, v in pairs(keys) do
         end
     end
 end
-ngx.log(ngx.WARN, values);
+-- ngx.log(ngx.WARN, values);
 
 local jwt = require("resty.jwt")
-local jwt_obj = jwt:load_jwt("eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IjdlYWtPdDMxbi1jS044OU9WbW1ISCJ9.eyJpc3MiOiJodHRwczovL2Rldi0wbjh4YnN5aXA3b20zdmF2LmV1LmF1dGgwLmNvbS8iLCJzdWIiOiJhdXRoMHw2NjBkOGY1YzQzMzJhZWVjMzgyNmEzMmQiLCJhdWQiOlsiaHR0cDovLzEyNy4wLjAuMTo4MCIsImh0dHBzOi8vZGV2LTBuOHhic3lpcDdvbTN2YXYuZXUuYXV0aDAuY29tL3VzZXJpbmZvIl0sImlhdCI6MTcxMjczODQ2MCwiZXhwIjoxNzEyNzM5MzYwLCJzY29wZSI6Im9wZW5pZCBwcm9maWxlIGVtYWlsIG9mZmxpbmVfYWNjZXNzIiwiYXpwIjoiSnVEYW82M3AwQnRIOVcydXVUbFpJeGZnbExUNVdVUnQiLCJwZXJtaXNzaW9ucyI6W119.LIS9BSM8Y1dgRl3Z-LhmMeT9U6FiovBdJDiocA-SA08MHFEhaJQ3Ye7szchGEnNmTlsCkXAVOQE4h-3P4XlsVwa5QR2CWUEY8W2fAel7841CYsirKsc5VBNcX5mFJ2P8CKqi_g-V-kxG5QhrVoo3XXZ9GdbMcs58EZ3oWRHx9nSO0Zw1uqjFofIU7QADMtWmaCsVU9xy6_OV9o24vg0ulhdRowk7xE2q-QsrUuJgfpZUFzGnMBjAZEfloTcSqcw4GtWJnHOtwrCqHeCvPGfrAfc2iIA5rbl3VAgLOM2jp7c0aSzdBfR4HJ9-Juld9eji4hKsCLapkDOiD-UfSRBSFQ")
-local access_token = ''
+local validators = require("resty.jwt-validators")
+local jwt_obj = jwt:load_jwt("token")
+ngx.log(ngx.WARN, "\n" .. cjson.encode(jwt_obj) .. "\n");
+local cert = "verifierer"
+local secret = "-----BEGIN CERTIFICATE-----\n$" .. cert .. "\n-----END CERTIFICATE-----\n";
+local verified = cjson.encode(jwt:verify_jwt_obj(secret, jwt_obj, {
+    valid_issuers = { "my-trusted-issuer", "my-other-trusteed-issuer" }
+}));
+ngx.log(ngx.WARN, "\n Is verified: " .. verified .. "\n");
+-- local access_token = ''
 -- for k, v in jwt_obj do
 --     if type(v) == 'string' then
 --         access_token = access_token .. "\n" .. v
 --     end
 -- end
 -- ngx.log(ngx.WARN, access_token)
-ngx.log(ngx.WARN, "\n" .. cjson.encode(jwt_obj) .. "\n");
+-- ngx.log(ngx.WARN, "\n" .. cjson.encode(jwt_obj) .. "\n");
