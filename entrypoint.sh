8388b5efc1718ecb1222c0bf6e07728bcfde9c28
jarnoweemen <jarno.weemen@gmail.com>
2024-04-14 13:28:11 +0200

    Update env variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 # Replace ENV placeholders with values
 envsubst < /etc/nginx/conf.d/server.conf.template > /etc/nginx/conf.d/server.conf '${DEFAULT_HOST} ${ACCOUNT_HOST}'
-envsubst < /usr/local/openresty/nginx/html/js/templates/client.template.js > /usr/local/openresty/nginx/html/js/client.js
+envsubst < /usr/local/openresty/nginx/html/js/templates/client.template.js > /usr/local/openresty/nginx/html/js/auth/client.js
 
 # In order for NGINX to run from docker file
 exec "$@"
