ce074821cdc3abe7715f0b5825fe0e814100932f
jarnoweemen <jarno.weemen@gmail.com>
2024-05-14 22:55:37 +0200

    Health check account

---
+++ health_check.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Endpoint URL
+URL="http://account:8080/account/user/balance?sub=${SUB_NOT_VERIFIED}"
+
+# Wait for the endpoint to return a 200 response
+until curl --fail --silent --output /dev/null "$URL"; do
+    sleep 5
+    echo "Retrying"
+done
+
+echo "Endpoint is up!"
