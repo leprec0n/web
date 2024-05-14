e7c19e40922482301bbfd61ffc440983ee092e13
jarnoweemen <jarno.weemen@gmail.com>
2024-05-14 21:09:04 +0200

    Open 443

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "https://web",
+    baseUrl: "https://127.0.0.1",
     video: false,
     setupNodeEvents(on, config) {
       require("@cypress/code-coverage/task")(on, config);
